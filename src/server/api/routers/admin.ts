f16a1444dd95028d5a9bebfa94cd66672ab100a0
evakotik1 <idrsovaanna@gmail.com>
2025-11-28 02:52:12 +0300

    redis

--- src/server/api/routers/admin.ts
+++ src/server/api/routers/admin.ts
@@ -4,6 +4,7 @@ import z from "zod/v4";
 import { userServise } from "./user";
 import { db } from "../../db";
 import { user } from "../../db/auth-schema";
+import { DEFAULT_TTL, ServeCached } from "../../redis";
 
 export const adminRouter = new Elysia({
     prefix: "/admin"
@@ -11,15 +12,18 @@ export const adminRouter = new Elysia({
 .use(userServise)
 
 .get("/users", async () => {
-    return await db.query.user.findMany({
-        where: isNull(user.deletedAt)
-    })
+    return await ServeCached(["admin", "users"], DEFAULT_TTL, async () => 
+        await db.query.user.findMany({
+            where: isNull(user.deletedAt)
+        })
+    )
 }, {
     whichRole: "admin"
 })
 
-.delete("/users/:id", async ({ params }) => {
-    return await db.delete(user).where(eq(user.id, params.id)).returning()
-}, {
-    whichRole: "admin"
-})
+// .delete("/users/:id", async ({ params }) => {
+//     console.log ({params})
+//     return await db.delete(user).where(eq(user.id, params.id)).returning()
+// }, {
+//     whichRole: "admin"
+// })
--- src/server/api/routers/products.ts
+++ src/server/api/routers/products.ts
@@ -5,20 +5,22 @@ import { userServise } from "./user";
 import { products } from "../../db/schema";
 import { db } from "../../db";
 import { productSchema } from "@/src/lib/client/shared/schemas/products";
+import { DEFAULT_TTL, InvalidateCached, ServeCached } from "../../redis";
 
 export const productsRouter = new Elysia({
     prefix: "/products"
 })
 .use(userServise)
 .get("/", async () => {
-    return await db.query.products.findMany({
+    return await ServeCached(["products"], DEFAULT_TTL, async () => 
+        await db.query.products.findMany({
         where: isNull(products.deletedAt)
-    })
+    }))
 })
 .get("/:id", async ({ params }) => {
-    return await db.query.products.findFirst({
+    return await ServeCached(["products", params.id], DEFAULT_TTL, async () => await db.query.products.findFirst({
         where: eq(products.id, params.id)
-    })
+    }));
 }, {
     params: z.object({
         id: z.string()
@@ -34,12 +36,18 @@ export const productsRouter = new Elysia({
     })
 })
 .post("/", async ({body}) => {
-    return await db.insert(products).values(body).returning()
+    const result = await db.insert(products).values(body).returning();
+    await InvalidateCached(["products"]);
+    return result;
+    
 }, {
-    body: productSchema
+    body: productSchema,
 })
+
 .put("/:id", async ({params, body}) => {
-    return await db.update(products).set(body).where(eq(products.id, params.id)).returning()
+    const result = await db.update(products).set(body).where(eq(products.id, params.id)).returning()
+    await InvalidateCached(["products"]);
+    return result;
 }, {
     body: productSchema,
     params: z.object({
